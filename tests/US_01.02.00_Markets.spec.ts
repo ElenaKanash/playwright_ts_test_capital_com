ef6282e799e1814e7c8200d7afbe27bb0af7a2a3
ElenaKanash <hird250@gmail.com>
2024-07-16 11:38:17 +0300

    add  US_01.02.00_Markets.spec.js

---
+++ tests/US_01.02.00_Markets.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from '@playwright/test';
+
+import HomePage from '../pages/homePage';
+import Header from '../pages/Header';
+import Markets from '../pages/elements/Markets';
+import SignUp from '../pages/elements/SignUp';
+
+const license = 'FCA';
+
+test.describe(`US_01.02!00 | Menu [Markets] > Menu item [Shares], ${license} license`, () => {
+  test.beforeEach(async ({ page }) => {
+    const homePage = new HomePage(page);
+    const header = new Header(page);
+
+    await homePage.openMainPageFCA();
+    await homePage.clickAcceptAllCookiesBtn();
+    await header.hoverMarketsMenu();
+    await header.clickMarketsSharesSubMenu();
+  });
+
+  test.describe('US_01.02!00_UnReg,', () => {
+    test.beforeEach(async ({ page }) => {
+      // Действия для незарегистрированного пользователя
+    });
+
+    test('TC_01.02!00_101_UnReg | Markets > Menu item [Shares] > Click button [Sign up] in the block "Shares trading"', async ({
+      page,
+    }) => {
+      const markets = new Markets(page);
+      const signUp = new SignUp(page);
+
+      await markets.clickMarketsTradingBlockSignUpBtn();
+      await expect(signUp.getSignUpForm).toBeVisible();
+      await signUp.verifySignUpForm();
+    });
+  });
+
+  test.describe('US_01.02!00_UnAuth,', () => {
+    test.beforeEach(async ({ page }) => {
+      // Действия для неавторизованного пользователя
+    });
+
+    test('TC_01.02!00_101_UnAuth | Markets > Menu item [Shares] > Click button [Sign up] in the block "Shares trading"', async ({
+      page,
+    }) => {
+      const markets = new Markets(page);
+      const signUp = new SignUp(page);
+
+      await markets.clickMarketsTradingBlockSignUpBtn();
+      await expect(signUp.getSignUpForm).toBeVisible();
+      await signUp.verifySignUpForm();
+    });
+  });
+
+  test.describe('US_01.02!00_Auth,', () => {
+    test.beforeEach(async ({ page }) => {
+      // Действия для авторизованного пользователя
+    });
+
+    test('TC_01.02!00_101_Auth | Markets > Menu item [Shares] > Click button [Sign up] in the block "Shares trading"', async ({
+      page,
+    }) => {
+      const markets = new Markets(page);
+
+      await markets.clickMarketsTradingBlockSignUpBtn();
+    });
+  });
+});
