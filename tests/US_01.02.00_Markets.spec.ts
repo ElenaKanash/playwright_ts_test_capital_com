ae228a63468c2cd0fbc99f2f47b9142b05791c82
Elena Kanash <78100493+ElenaKanash@users.noreply.github.com>
2024-07-18 14:59:43 +0300

    add TC_01.02!00_101_UnAuth
    * add US_01.02!00
    
    * add precondition and  steps to test('TC_01.02!00_101_UnAuth)
    
    * add precondition and steps to test('TC_01.02!00_101_UnAuth)
    
    * update imports
    
    * change import
    
    * delete test.beforeEach
    
    * refactor clickMarketsSharesMenuItem()
    
    * add  US_01.02.00_Markets.spec.js
    
    * fix  clickMarketsSharesMenuItem();
    
    * add beforeEach for autorizedUser
    
    * change getHeaderMyAccountBtn
    
    * add TC_01.02!00_101_UnAuth
    
    * fix error beforeEach
    
    * increase timeout
    
    * add test.slow  to US_01.02!00
    
    * remove test.slow

--- tests/US_01.02.00_Markets.spec.ts
+++ tests/US_01.02.00_Markets.spec.ts
@@ -8,20 +8,20 @@ import Login from '../pages/elements/Login';
 
 const license = 'FCA';
 
-test.describe(`US_01.02!00 | Menu [Markets] > Menu item [Shares], ${license} license`, () => {
-  test.beforeEach(async ({ page }) => {
+test.describe(`US_01.02!00 | Menu [Markets] > Menu item [Shares], ${license} license`, () => {  
+  test.beforeEach(async ({ page }) => {    
     const homePage = new HomePage(page);
-    const header = new Header(page);
 
     await homePage.openMainPageFCA();
     await homePage.clickAcceptAllCookiesBtn();
-    await header.hoverMarketsMenu();
-    await header.clickMarketsSharesMenuItem();
   });
 
   test.describe('US_01.02!00_UnReg | Menu [Markets] > Menu item [Shares]', () => {
     test.beforeEach(async ({ page }) => {
-      //actions for unreg user
+      const header = new Header(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsSharesMenuItem();
     });
 
     test('TC_01.02!00_101_UnReg | Markets > Menu item [Shares] > Click button [Sign up] in the block "Shares trading"', async ({
@@ -36,10 +36,14 @@ test.describe(`US_01.02!00 | Menu [Markets] > Menu item [Shares], ${license} lic
     });
   });
 
-/*   test.describe('US_01.02!00_UnAuth | Menu [Markets] > Menu item [Shares],', () => {
+  test.describe('US_01.02!00_UnAuth | Menu [Markets] > Menu item [Shares],', () => {
     test.beforeEach(async ({ page }) => {
       const login = new Login(page);
-      //await login.unAutorizedUser();
+      const header = new Header(page);
+
+      await login.unAutorizedUser();
+      await header.hoverMarketsMenu();
+      await header.clickMarketsSharesMenuItem();
     });
 
     test('TC_01.02!00_101_UnAuth | Markets > Menu item [Shares] > Click button [Sign up] in the block "Shares trading"', async ({
@@ -54,21 +58,30 @@ test.describe(`US_01.02!00 | Menu [Markets] > Menu item [Shares], ${license} lic
     });
   });
 
-  test.describe('US_01.02!00_Auth | Menu [Markets] > Menu item [Shares],', () => {
+  /* test.describe('US_01.02!00_Auth | Menu [Markets] > Menu item [Shares],', () => {
     test.beforeEach(async ({ page }) => {
       const login = new Login(page);
+      const header = new Header(page);
+
       await login.autorizedUser();
+      await header.hoverMarketsMenu();
+      await header.clickMarketsSharesMenuItem();
+
+
+
     });
 
     test('TC_01.02!00_101_Auth | Markets > Menu item [Shares] > Click button [Sign up] in the block "Shares trading"', async ({
       page,
     }) => {
       const markets = new Markets(page);
+
       const login = new Login(page);
 
       await markets.clickMarketsTradingBlockSignUpBtn();
       await expect(page).toHaveURL('https://capital.com/trading/platform/');
       //await login.checkNavigationToPlatform();
+
     });
   }); */
 });
