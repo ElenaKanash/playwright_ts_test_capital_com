e68e691cd02cf7696b9eab972fc1b62c91b3bb9c
ElenaKanash <hird250@gmail.com>
2024-10-15 11:08:48 +0300

    add TC_01.00!00_102

--- tests/US_01.00,00_Markets.spec.ts
+++ tests/US_01.00,00_Markets.spec.ts
@@ -7,7 +7,7 @@ import MarketsPage from '../pages/marketsPage.js';
 const license = 'FCA';
 const language = 'EN';
 
-test.describe(`US_01.00!0 | Menu Markets > Menu section [Markets] , ${license} license, ${language} language`, () => {
+test.describe(`US_01.00!00 | Menu Markets > Menu section [Markets] , ${license} license, ${language} language`, () => {
   test.beforeEach(async ({ page }) => {
     const homePage = new HomePage(page);
 
@@ -15,7 +15,7 @@ test.describe(`US_01.00!0 | Menu Markets > Menu section [Markets] , ${license} l
     await homePage.clickAcceptAllCookiesBtn();
   });
 
-  test.describe('TC_01.00!00_101 | Markets >Menu section [Markets] > Click button [Create account] in the block "Access thousands of global markets"', () => {
+  test.describe('TC_01.00!00_101 | Markets > Menu section [Markets] > Click button [Create account] in the block "Access thousands of global markets"', () => {
 
     test('TC_01.00!00_101_UnReg', async ({ page }) => {
       const header = new Header(page);
@@ -48,6 +48,42 @@ test.describe(`US_01.00!0 | Menu Markets > Menu section [Markets] , ${license} l
       await header.clickMarketsMenuSectionTitle();
       await markets.clickAccessBlockCreateAccountBtn();
       await expect.tradingPlatformIsOpened();
+    });
+  });
+
+  test.describe('TC_01.00!00_102 | Markets > Menu section [Markets] > Click button [Try demo account] in the block "Access thousands of global markets"', () => {
+
+    test('TC_01.00!00_102_UnReg', async ({ page }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsMenuSectionTitle();
+      await markets.clickAccessBlockTryDemoAccountBtn();
+      await expect.signUpFormIsOpened();
+    });
+
+    test('TC_01.00!00_102_UnAuth', async ({ page, unAutorizedUserRole }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsMenuSectionTitle();
+      await markets.clickAccessBlockTryDemoAccountBtn();
+      await expect.loginFormIsOpened();
+    });
+
+    test('TC_01.00!00_102_Auth', async ({ page, autorizedUserRole }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsMenuSectionTitle();
+      await markets.clickAccessBlockTryDemoAccountBtn();
+      await expect.tradingPlatformDemoModeIsOpened();
     });
   });
 });
