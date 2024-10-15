bf499ca6c8a271b755ac54812963ae359e4c572e
ElenaKanash <hird250@gmail.com>
2024-10-15 11:09:11 +0300

    add clickAccessBlockTryDemoAccountBtn ()

--- pages/marketsPage.ts
+++ pages/marketsPage.ts
@@ -3,16 +3,22 @@ import { Locator, Page } from '@playwright/test';
 class MarketsPage {
   readonly page: Page;
   readonly getAccessBlockCreateAccountBtn: Locator;
+  readonly getAccessBlockTryDemoAccountBtn: Locator;
   //readonly : Locator; 
 
   constructor(page: Page) {
     this.page = page;
-    //this.getMarketsMenu = page.locator('header a[href="/en-gb/markets"]');
+
     this.getAccessBlockCreateAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Create account' });
+    this.getAccessBlockTryDemoAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Try demo account' });
   }
 
-  async clickAccessBlockCreateAccountBtn () {
+  async clickAccessBlockCreateAccountBtn() {
     await this.getAccessBlockCreateAccountBtn.click();
+  }
+
+  async clickAccessBlockTryDemoAccountBtn() {
+    await this.getAccessBlockTryDemoAccountBtn.click();
   }
 }
 export default MarketsPage;
