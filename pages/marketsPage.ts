d9c5bd1e5c24a825c5e90b9a868d429ceeb4c50b
ElenaKanash <hird250@gmail.com>
2024-10-22 13:26:57 +0300

    add clickTradingBlockCreateAccountBtn()

--- pages/marketsPage.ts
+++ pages/marketsPage.ts
@@ -6,6 +6,7 @@ class MarketsPage {
   readonly getMarketsBlockTryDemoAccountBtn: Locator;
   readonly getWhyChooseCapitalBlockCreateAccountBtn: Locator;
   readonly getWhyChooseCapitalBlockTryDemoBtn: Locator;
+  readonly getTradingBlockCreateAccountBtn: Locator
   //readonly : Locator; 
 
   constructor(page: Page) {
@@ -13,6 +14,7 @@ class MarketsPage {
 
     this.getMarketsBlockCreateAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Create account' });
     this.getMarketsBlockTryDemoAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Try demo account' });
+    this.getTradingBlockCreateAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Create account' });
     this.getWhyChooseCapitalBlockCreateAccountBtn = page.getByRole('button', { name: 'Create account' }).nth(1);
     this.getWhyChooseCapitalBlockTryDemoBtn = page.getByRole('button', { name: 'Try Demo', exact: true });
   }
@@ -31,6 +33,10 @@ class MarketsPage {
 
   async clickWhyChooseCapitalBlockTryDemoBtn() {
     await this.getWhyChooseCapitalBlockTryDemoBtn.click();
+  }
+
+  async clickTradingBlockCreateAccountBtn() {
+    await this.getTradingBlockCreateAccountBtn.click();
   }
 }
 export default MarketsPage;
