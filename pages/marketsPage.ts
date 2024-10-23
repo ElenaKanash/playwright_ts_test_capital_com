307f091f6b2179d2d32bcfa600c8bc3ad6d9071d
ElenaKanash <hird250@gmail.com>
2024-10-23 11:19:30 +0300

    add clickTradingBlockTryDemoBtn()

--- pages/marketsPage.ts
+++ pages/marketsPage.ts
@@ -6,7 +6,8 @@ class MarketsPage {
   readonly getMarketsBlockTryDemoAccountBtn: Locator;
   readonly getWhyChooseCapitalBlockCreateAccountBtn: Locator;
   readonly getWhyChooseCapitalBlockTryDemoBtn: Locator;
-  readonly getTradingBlockCreateAccountBtn: Locator
+  readonly getTradingBlockCreateAccountBtn: Locator;
+  readonly getTradingBlockTryDemoBtn: Locator;
   //readonly : Locator; 
 
   constructor(page: Page) {
@@ -15,6 +16,7 @@ class MarketsPage {
     this.getMarketsBlockCreateAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Create account' });
     this.getMarketsBlockTryDemoAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Try demo account' });
     this.getTradingBlockCreateAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Create account' });
+    this.getTradingBlockTryDemoBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Try demo' });
     this.getWhyChooseCapitalBlockCreateAccountBtn = page.getByRole('button', { name: 'Create account' }).nth(1);
     this.getWhyChooseCapitalBlockTryDemoBtn = page.getByRole('button', { name: 'Try Demo', exact: true });
   }
@@ -37,6 +39,16 @@ class MarketsPage {
 
   async clickTradingBlockCreateAccountBtn() {
     await this.getTradingBlockCreateAccountBtn.click();
+  }
+
+  async clickTradingBlockTryDemoBtn() {
+    if (this.getTradingBlockTryDemoBtn) {
+      await this.getTradingBlockTryDemoBtn.click();
+    } else {
+      throw new Error(
+        "Bug! Button [Try Demo] is absent"
+      );
+    }
   }
 }
 export default MarketsPage;
