6a352c1ad6630bed9aa1272bf5ff543e6907377e
ElenaKanash <hird250@gmail.com>
2024-12-24 13:58:44 +0300

    add clickMarketsTradingBlockTryDemoBtn();

--- pages/marketsPage.ts
+++ pages/marketsPage.ts
@@ -7,10 +7,10 @@ class MarketsPage {
   readonly getWhyChooseCapitalBlockCreateAccountBtn: Locator;
   readonly getWhyChooseCapitalBlockTryDemoBtn: Locator;
   readonly getTradingBlockCreateAccountBtn: Locator;
-  //readonly getTradingBlockTryDemoBtn: Locator;
+  readonly getMarketsTradingBlockTryDemoBtn: Locator;
   readonly getDiscoverTradingBlockCreateAccountBtn: Locator;
   getTradingBlockTryDemoBtn(): Locator {
-    return this.page.locator('#bannerFullScreen').getByRole('button', { name: 'Try demo' });
+    return this.page.locator('#bannerFullScreen').getByRole('button', { name: 'Try Demo' });
   }
 
   //readonly : Locator; 
@@ -21,7 +21,7 @@ class MarketsPage {
     this.getMarketsBlockCreateAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Create account' });
     this.getMarketsBlockTryDemoAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Try demo account' });
     this.getTradingBlockCreateAccountBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Create account' });
-    //this.getTradingBlockTryDemoBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Try demo' });
+    this.getMarketsTradingBlockTryDemoBtn = page.locator('#bannerFullScreen').getByRole('button', { name: 'Try Demo' });
     //blocks at the bottom of the page
     this.getWhyChooseCapitalBlockCreateAccountBtn = page.getByRole('button', { name: 'Create account' }).nth(1);
     this.getWhyChooseCapitalBlockTryDemoBtn = page.getByRole('button', { name: 'Try Demo', exact: true });
@@ -36,6 +36,10 @@ class MarketsPage {
 
   async clickMarketsBlockTryDemoAccountBtn() {
     await this.getMarketsBlockTryDemoAccountBtn.click();
+  }
+
+  async clickMarketsTradingBlockTryDemoBtn() {
+    await this.getMarketsTradingBlockTryDemoBtn.click();
   }
 
   async clickWhyChooseCapitalBlockCreateAccountBtn() {
