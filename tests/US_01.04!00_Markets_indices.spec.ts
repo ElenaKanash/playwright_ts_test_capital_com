233682003f78a8e8ae453081adcf1235dd2579db
ElenaKanash <hird250@gmail.com>
2024-12-24 13:38:57 +0300

    add TC_01.04!00_101

---
+++ tests/US_01.04!00_Markets_indices.spec.ts
@@ -0,0 +1,59 @@
+import { test } from './base.js';
+import { Assertions } from '../pages/assertionsForTests.js';
+import HomePage from '../pages/homePage.js';
+import Header from '../pages/headerPage.js';
+import MarketsPage from '../pages/marketsPage.js';
+import TableTradingInstruments from '../pages/elements/tableTradingInstruments.js';
+
+const license = 'FCA';
+const language = 'EN';
+
+test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, ${language} language`, () => {
+
+  test.beforeEach(async ({ page }) => {
+    const homePage = new HomePage(page);
+
+    await homePage.openMainPageFCA();    
+    await homePage.clickAcceptAllCookiesBtn();
+    await homePage.clickWelcomeBanner();
+  });
+
+  test.describe('TC_01.04!00_101 | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', () => {
+
+    test('TC_01.04!00_101_UnReg | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({ page }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickTradingBlockCreateAccountBtn();
+
+      await expect.signUpFormIsOpened();
+    });
+
+    test('TC_01.04!00_101_UnAuth | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({ page, unAutorizedUserRole }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickTradingBlockCreateAccountBtn();
+
+      await expect.signUpFormIsOpened();
+    });
+
+    test('TC_01.04!00_101_Auth | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({ page, autorizedUserRole }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickTradingBlockCreateAccountBtn();
+
+      await expect.tradingPlatformIsOpened();
+    });
+  });
+});
