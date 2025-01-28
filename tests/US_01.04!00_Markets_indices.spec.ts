993e0f428b016faf95b9c71a0f3a5af4e08597b6
ElenaKanash <hird250@gmail.com>
2025-01-28 23:13:25 +0300

    chanfe workers: process.env.CI ? 2

--- tests/US_01.04!00_Markets_indices.spec.ts
+++ tests/US_01.04!00_Markets_indices.spec.ts
@@ -9,18 +9,18 @@ const license = 'FCA';
 const language = 'EN';
 
 test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, ${language} language`, () => {
-
   test.beforeEach(async ({ page }) => {
     const homePage = new HomePage(page);
 
-    await homePage.openMainPageFCA();    
+    await homePage.openMainPageFCA();
     await homePage.clickAcceptAllCookiesBtn();
-    await homePage.clickWelcomeBanner();
+    //await homePage.clickWelcomeBanner();
   });
 
   test.describe('TC_01.04!00_101 | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', () => {
-
-    test('TC_01.04!00_101_UnReg | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({ page }) => {
+    test('TC_01.04!00_101_UnReg | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({
+      page,
+    }) => {
       const header = new Header(page);
       const markets = new MarketsPage(page);
       const expect = new Assertions(page);
@@ -32,7 +32,10 @@ test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, 
       await expect.signUpFormIsOpened();
     });
 
-    test('TC_01.04!00_101_UnAuth | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({ page, unAutorizedUserRole }) => {
+    test('TC_01.04!00_101_UnAuth | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({
+      page,
+      unAutorizedUserRole,
+    }) => {
       const header = new Header(page);
       const markets = new MarketsPage(page);
       const expect = new Assertions(page);
@@ -44,7 +47,10 @@ test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, 
       await expect.signUpFormIsOpened();
     });
 
-    test('TC_01.04!00_101_Auth | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({ page, autorizedUserRole }) => {
+    test('TC_01.04!00_101_Auth | Markets > Menu item [Indices] > Click button [Create account] in the block "Indices trading"', async ({
+      page,
+      autorizedUserRole,
+    }) => {
       const header = new Header(page);
       const markets = new MarketsPage(page);
       const expect = new Assertions(page);
@@ -58,8 +64,9 @@ test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, 
   });
 
   test.describe('TC_01.04!00_102 | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Indices trading"', () => {
-
-    test('TC_01.04!00_102_UnReg | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Indices trading"', async ({ page }) => {
+    test('TC_01.04!00_102_UnReg | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Indices trading"', async ({
+      page,
+    }) => {
       const header = new Header(page);
       const markets = new MarketsPage(page);
       const expect = new Assertions(page);
@@ -71,7 +78,10 @@ test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, 
       await expect.signUpFormIsOpened();
     });
 
-    test('TC_01.04!00_102_UnAuth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Indices trading"', async ({ page, unAutorizedUserRole }) => {
+    test('TC_01.04!00_102_UnAuth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Indices trading"', async ({
+      page,
+      unAutorizedUserRole,
+    }) => {
       const header = new Header(page);
       const markets = new MarketsPage(page);
       const expect = new Assertions(page);
@@ -83,7 +93,10 @@ test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, 
       await expect.loginFormIsOpened();
     });
 
-    test('TC_01.04!00_102_Auth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Indices trading"', async ({ page, autorizedUserRole }) => {
+    test('TC_01.04!00_102_Auth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Indices trading"', async ({
+      page,
+      autorizedUserRole,
+    }) => {
       const header = new Header(page);
       const markets = new MarketsPage(page);
       const expect = new Assertions(page);
@@ -94,115 +107,136 @@ test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, 
 
       await expect.tradingPlatformDemoModeIsOpened();
     });
+  });
 
-    test.describe('TC_01.04!00_103 | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', () => {
+  test.describe('TC_01.04!00_103 | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', () => {
+    test('TC_01.04!00_103_UnReg | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', async ({
+      page,
+    }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
 
-      test('TC_01.04!00_103_UnReg | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', async ({ page }) => {
-        const header = new Header(page);
-        const markets = new MarketsPage(page);
-        const expect = new Assertions(page);
-  
-        await header.hoverMarketsMenu();
-        await header.clickMarketsIndicesMenuItem();
-        await markets.clickDiscoverTradingBlockCreateAccountBtn();
-  
-        await expect.signUpFormIsOpened();
-      });
-  
-      test('TC_01.04!00_103_UnAuth | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', async ({ page, unAutorizedUserRole }) => {
-        const header = new Header(page);
-        const markets = new MarketsPage(page);
-        const expect = new Assertions(page);
-  
-        await header.hoverMarketsMenu();
-        await header.clickMarketsIndicesMenuItem();
-        await markets.clickDiscoverTradingBlockCreateAccountBtn();
-  
-        await expect.signUpFormIsOpened();
-      });
-  
-      test('TC_01.04!00_103_Auth | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', async ({ page, autorizedUserRole }) => {
-        const header = new Header(page);
-        const markets = new MarketsPage(page);
-        const expect = new Assertions(page);
-  
-        await header.hoverMarketsMenu();
-        await header.clickMarketsIndicesMenuItem();
-        await markets.clickDiscoverTradingBlockCreateAccountBtn();
-  
-        await expect.tradingPlatformIsOpened();
-      });
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickDiscoverTradingBlockCreateAccountBtn();
+
+      await expect.signUpFormIsOpened();
     });
 
-    test.describe('TC_01.04!00_104 | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', () => {
+    test('TC_01.04!00_103_UnAuth | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', async ({
+      page,
+      unAutorizedUserRole,
+    }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
 
-      test('TC_01.04!00_104_UnReg | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', async ({ page }) => {
-        const header = new Header(page);
-        const markets = new MarketsPage(page);
-        const expect = new Assertions(page);
-  
-        await header.hoverMarketsMenu();
-        await header.clickMarketsIndicesMenuItem();
-        await markets.clickDiscoverTradingBlockTryDemoBtn();
-  
-        await expect.signUpFormIsOpened();
-      });
-  
-      test('TC_01.04!00_104_UnAuth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', async ({ page, unAutorizedUserRole }) => {
-        const header = new Header(page);
-        const markets = new MarketsPage(page);
-        const expect = new Assertions(page);
-  
-        await header.hoverMarketsMenu();
-        await header.clickMarketsIndicesMenuItem();
-        await markets.clickDiscoverTradingBlockTryDemoBtn();
-  
-        await expect.loginFormIsOpened();
-      });
-  
-      test('TC_01.04!00_104_Auth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', async ({ page, autorizedUserRole }) => {
-        const header = new Header(page);
-        const markets = new MarketsPage(page);
-        const expect = new Assertions(page);
-  
-        await header.hoverMarketsMenu();
-        await header.clickMarketsIndicesMenuItem();
-        await markets.clickDiscoverTradingBlockTryDemoBtn();
-  
-        await expect.tradingPlatformDemoModeIsOpened();
-      });
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickDiscoverTradingBlockCreateAccountBtn();
+
+      await expect.signUpFormIsOpened();
+    });
+
+    test('TC_01.04!00_103_Auth | Markets > Menu item [Indices] > Click button [Create account] in the block "Discover trading excellence with Capital.com"', async ({
+      page,
+      autorizedUserRole,
+    }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickDiscoverTradingBlockCreateAccountBtn();
+
+      await expect.tradingPlatformIsOpened();
+    });
+  });
+
+  test.describe('TC_01.04!00_104 | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', () => {
+    test('TC_01.04!00_104_UnReg | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', async ({
+      page,
+    }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickDiscoverTradingBlockTryDemoBtn();
+
+      await expect.signUpFormIsOpened();
+    });
+
+    test('TC_01.04!00_104_UnAuth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', async ({
+      page,
+      unAutorizedUserRole,
+    }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickDiscoverTradingBlockTryDemoBtn();
+
+      await expect.loginFormIsOpened();
+    });
+
+    test('TC_01.04!00_104_Auth | Markets > Menu item [Indices] > Click button [Try Demo] in the block "Discover trading excellence with Capital.com"', async ({
+      page,
+      autorizedUserRole,
+    }) => {
+      const header = new Header(page);
+      const markets = new MarketsPage(page);
+      const expect = new Assertions(page);
+
+      await header.hoverMarketsMenu();
+      await header.clickMarketsIndicesMenuItem();
+      await markets.clickDiscoverTradingBlockTryDemoBtn();
+
+      await expect.tradingPlatformDemoModeIsOpened();
     });
   });
 
   test.describe('TC_01.04!00_105 | Markets > Menu item [Indices] > Click random trading instruments on the Widget “Trading instrument”', () => {
-
-    test('TC_01.04!00_105_UnReg | Markets > Menu item [Indices] > Click random trading instruments on the Widget “Trading instrument”', async ({ page }) => {
-      const header = new Header(page);      
-      const tableTradingInstruments = new TableTradingInstruments(page);;
+    test('TC_01.04!00_105_UnReg | Markets > Menu item [Indices] > Click random trading instruments on the Widget “Trading instrument”', async ({
+      page,
+    }) => {
+      const header = new Header(page);
+      const tableTradingInstruments = new TableTradingInstruments(page);
 
       await header.hoverMarketsMenu();
       await header.clickMarketsIndicesMenuItem();
-      
+
       await tableTradingInstruments.clickRandomInstrumentInTheSortedTable();
     });
 
-    test.skip('TC_01.04!00_105_UnAuth | Markets > Menu item [Indices] > Click random trading instruments on the Widget “Trading instrument”"', async ({ page, unAutorizedUserRole }) => {
-      const header = new Header(page);      
-      const tableTradingInstruments = new TableTradingInstruments(page);;
+    test.skip('TC_01.04!00_105_UnAuth | Markets > Menu item [Indices] > Click random trading instruments on the Widget “Trading instrument”"', async ({
+      page,
+      unAutorizedUserRole,
+    }) => {
+      const header = new Header(page);
+      const tableTradingInstruments = new TableTradingInstruments(page);
 
       await header.hoverMarketsMenu();
       await header.clickMarketsIndicesMenuItem();
-      
+
       await tableTradingInstruments.clickRandomInstrumentInTheSortedTable();
     });
 
-    test.skip('TC_01.04!00_105_Auth | Markets > Menu item [Indices] > Click random trading instruments on the Widget “Trading instrument”', async ({ page, autorizedUserRole }) => {
-      const header = new Header(page);      
-      const tableTradingInstruments = new TableTradingInstruments(page);;
+    test.skip('TC_01.04!00_105_Auth | Markets > Menu item [Indices] > Click random trading instruments on the Widget “Trading instrument”', async ({
+      page,
+      autorizedUserRole,
+    }) => {
+      const header = new Header(page);
+      const tableTradingInstruments = new TableTradingInstruments(page);
 
       await header.hoverMarketsMenu();
       await header.clickMarketsIndicesMenuItem();
-      
+
       await tableTradingInstruments.clickRandomInstrumentInTheSortedTable();
     });
   });
@@ -220,8 +254,13 @@ test.describe(`US_01.04!00 | Markets > Menu item [Indices], ${license} license, 
     }) => {
       const tableTradingInstruments = new TableTradingInstruments(page);
 
-      await tableTradingInstruments.saveAllTableInstrumentLinksToFile(page, 'a', 'links.txt', 'Forex');
+      await tableTradingInstruments.saveAllTableInstrumentLinksToFile(
+        page,
+        'a',
+        'links.txt',
+        'Indices'
+      );
     });
   });
-
+  
 });
